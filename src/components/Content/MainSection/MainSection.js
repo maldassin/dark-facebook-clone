268d73d3fe913365ef85de9cfe51564f76ab5e16
maldassin <maendassin@gmail.com>
2023-03-22 10:00:29 +0300

    Made width dynamic + more similar to the real deal

--- src/components/Content/MainSection/MainSection.js
+++ src/components/Content/MainSection/MainSection.js
@@ -2,8 +2,8 @@ import { ImportContactsSharp, MovieFilterSharp } from "@mui/icons-material";
 
 function MainSection() {
   return (
-    <div className='main-section flex flex-1 grow justify-center px-8 pt-1'>
-      <div className='mt-2 flex w-[500px] flex-col items-center rounded-2xl border border-trueGray-800 bg-trueGray-800'>
+    <div className='main-section flex w-full justify-center flex-1 shrink-0'>
+      <div className='mt-4 flex w-[73%] flex-col items-center rounded-lg border border-trueGray-800 bg-trueGray-800'>
         <div className='main-section__options relative flex h-[72px] w-full border-b-2 border-trueGray-700'>
           <div
             className='ml-4 flex w-full'
@@ -11,7 +11,7 @@ function MainSection() {
               borderBottom: "3.5px solid #3B82F6",
             }}
           >
-            <div className='stories flex h-full w-full cursor-pointer justify-center items-center self-center rounded-lg hover:bg-trueGray-700'>
+            <div className='stories flex h-full w-full cursor-pointer items-center justify-center self-center rounded-lg'>
               <ImportContactsSharp
                 className='text-blue-400'
                 sx={{
@@ -19,7 +19,7 @@ function MainSection() {
                   height: 24,
                 }}
               />
-              <p className='ml-3 text-blue-400'>Stories</p>
+              <p className='ml-2 text-blue-400'>Stories</p>
             </div>
           </div>
           <div className='reels mr-4 ml-1 flex w-full cursor-pointer items-center justify-center rounded-lg pr-1 hover:bg-trueGray-700'>
@@ -30,7 +30,7 @@ function MainSection() {
                 height: 24,
               }}
             />
-            <p className='ml-3'>Reels</p>
+            <p className='ml-2'>Reels</p>
           </div>
         </div>
         <div className='main-section__stories'></div>
